01e0926c77c3a03908b258c9c5d855bdaf78c04c
Marshall <mn.acunab@gmail.com>
2023-09-29 15:26:59 -0300

    add swagger, swagger main conf

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Conf
+  const options = new DocumentBuilder()
+    .setTitle('Listado de facturas con swagger')
+    .setDescription('API REST para listado de facturas')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  // Swagger doc route
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
